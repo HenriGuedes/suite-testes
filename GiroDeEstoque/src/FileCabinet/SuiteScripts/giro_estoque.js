72522bd3be6691738bf653cbd28af3a920dfed04
henriGuedes <guedesxx24@gmail.com>
2026-07-17 14:52:43 -0300

    atualização do campo Produto

--- GiroDeEstoque/src/FileCabinet/SuiteScripts/giro_estoque.js
+++ GiroDeEstoque/src/FileCabinet/SuiteScripts/giro_estoque.js
@@ -6,30 +6,53 @@
  * @NScriptType Suitelet
  */
 
-define(['N/ui/serverWidget', 'N/search'],  (serverWidget, search) => {
+define(['N/ui/serverWidget', 'N/search'], (serverWidget, search) => {
 
     function onRequest(context) {
 
         var form = serverWidget.createForm({
             title: "Giro de Estoque"
         });
-        var bucaItem = search.create({
-            type: search.type.ITEM,
-            filters:["type", "anyof", "InvtPart"],
-            columns:[ "internalid", "itemid"]
-        })
-        form.addField({
+
+
+        var campoProduto = form.addField({
+            // cria o campo select para escolher o produto
             id: "custpage_produto",
-            type: serverWidget.FieldType.Select,
+            type: serverWidget.FieldType.SELECT,
             label: "Produto"
         })
+
+        // faz a busca de todos os itens do tipo inventario
+        var bucaItem = search.create({
+            type: search.Type.ITEM,
+            filters: ["type", "anyof", "InvtPart"],
+            columns: ["internalid", "itemid"]
+        })
+
+        var resultadoItens = bucaItem.run().getRange({
+            //ele pega os 1000 primeiros itens e os transforma em um array para poder percorrer e adicionar no select do campo produto
+            start: 0,
+            end: 1000
+        })
+
+        resultadoItens.forEach((item) => {
+            // for q percorre os itens e adiciona no select do campo produto 
+            campoProduto.addSelectOption({
+                value: item.getValue({ name: "internalid" }),
+                text: item.getValue({ name: "itemid" })
+            })
+
+        })
+
+
         var produtoEscolhido = context.request.parameters.custpage_produto
         form.addButton({
+            // botao de busca mais ainda sem funcionalidade
             id: "custpage_btn_pesquisa",
             label: "Pesquisar"
         })
 
-        context.response.writePage(form);
+        context.response.writePage(form); // imprina na tela o formulario criado 
 
 
 
